dcdf34f602953be48e3485773b834bc30b1bb96f
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-29 19:40:59 +0200

    task-02

---
+++ src/js/02-timer.js
@@ -0,0 +1,119 @@
+// const date = new Date()
+// console.log(typeof date, date);
+// const dateString = date.toString()
+// console.log(typeof dateString, dateString);
+// console.log(new Date(0));
+// console.log(date.getTime());
+
+// const teamMeetingDate = new Date("March 16, 2030")
+// console.log(teamMeetingDate);
+// console.log(date.getMinutes());
+// console.log(date.getHours());
+
+import flatpickr from "flatpickr";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    selector: document.querySelector('input[type = "text"]'),
+    startBtn: document.querySelector('button'),
+    days: document.querySelectorAll('.field ')[0].children[0],
+    hours: document.querySelectorAll('.field ')[1].children[0],
+    minutes: document.querySelectorAll('.field ')[2].children[0],
+    seconds: document.querySelectorAll('.field ')[3].children[0]
+}
+// console.log(refs.selector);
+// console.log(refs.startBtn);
+// console.log(refs.days);
+// console.log(refs.seconds);
+
+refs.startBtn.disabled = true;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        // console.log(selectedDates[0]);
+        const date = new Date();
+        const userDate = selectedDates[0];
+        dateAlert(userDate, date)
+        if (refs.startBtn.disabled === false) {
+            const ms = dateCompare(userDate, date)
+            startTimer(ms)
+        }
+    },
+};
+
+flatpickr(refs.selector, options)
+
+function dateAlert(userDate, date) {
+    console.log(`current date: ${date}`);
+    console.log(`user date: ${userDate}`);
+    if (date >= userDate) {
+        return Notify.failure('Please choose a date in the future');
+    }
+    refs.startBtn.disabled = false;
+}
+
+function dateCompare(userDate, date) {
+    console.log('compare');
+    const differenceMs = userDate - date
+    console.log(`time left in ms:  ${differenceMs}`);
+    console.log("time left converted: " + JSON.stringify(convertMs(differenceMs)));
+    changeValues(differenceMs)
+    return differenceMs
+}
+
+function startTimer(ms) {
+    const startTime = Date.now()
+    const finishTime = startTime + ms
+    console.log(`finish time: ${finishTime}`);
+    let timer = setInterval(() => {
+        const currentTime = Date.now()
+        if (currentTime < finishTime) {
+            const deltaTime = currentTime - startTime
+            const timeLeft = finishTime - currentTime
+            changeValues(timeLeft)
+            console.log(currentTime);
+            console.log(deltaTime);
+        } else {
+            clearInterval(timer)
+        }
+    }, 1000)
+
+}
+
+function changeValues(ms) {
+    const timeLeft = convertMs(ms)
+    console.log(timeLeft);
+    refs.days.innerText = timeLeft.days
+    refs.hours.innerText = timeLeft.hours
+    refs.minutes.innerText = timeLeft.minutes
+    refs.seconds.innerText = timeLeft.seconds
+}
+
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = pad(Math.floor(ms / day));
+    // Remaining hours
+    const hours = pad(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+}
+
+function pad(value) {
+    return String(value).padStart(2, '0')
+}
