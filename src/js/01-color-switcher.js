d265c920d29bfaa524092b95a9236e65e72e5327
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-29 10:55:49 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+console.log('color-switcher');
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+}
+let timerId = null;
 
+console.log(refs.startBtn);
+console.log(refs.stopBtn);
+
+refs.stopBtn.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBackgroundColor() {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`
+}
+
+refs.startBtn.addEventListener('click', startChangeColor)
+refs.stopBtn.addEventListener('click', stopChangeColor)
+
+function startChangeColor(e) {
+    console.log('test');
+    changeBodyBackgroundColor()
+    timerId = setInterval(() => {
+        changeBodyBackgroundColor()
+        console.log(timerId);
+    }, 1000)
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function stopChangeColor(e) {
+    console.log('stop test');
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+
+console.log('test');
+console.log('test');
